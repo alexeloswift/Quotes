5f473b8abede06a555c1550ce53e9bc2a104cc59
Alex Diaz <alexelo.swift@gmail.com>
2022-10-31 22:52:04 -0400

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Alexis Diaz on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
